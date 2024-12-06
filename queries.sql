d8998beaef63af5163ade98461c831830124f9ba
jyoti-bisht-7 <jyotibisht71320@gmail.com>
2024-12-06 18:47:59 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,16 @@
+create database bus;
+
+use bus;
+
+create table student(
+  student_id int primary key,
+  password varchar(20));
+
+insert into student student_id values (230104702);
+
+insert into student (student_id) values (230121625);
+
+insert into student (student_id) values (230121875);
+
+
 
