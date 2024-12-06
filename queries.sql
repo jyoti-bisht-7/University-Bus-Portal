7f06f91d5eeac083049ce12da19a6798548bf192
jyoti-bisht-7 <jyotibisht71320@gmail.com>
2024-12-06 18:51:58 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -12,5 +12,15 @@ insert into student (student_id) values (230121625);
 
 insert into student (student_id) values (230121875);
 
+create table bus_card_details(
+  Name varchar(20),
+  Student_ID  int,
+  Father_name varchar(20),
+  Course varchar(10),
+  Route varchar(50),
+  Phone_no varchar(10),
+  Bus_no int,
+  Seat_no int));
+  
 
 
